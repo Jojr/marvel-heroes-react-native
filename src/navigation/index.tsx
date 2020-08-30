61ba22cef82b99a2656bc0bf77af914275baf263
João Belem Jr <junior.jb@gmail.com>
2020-08-30 02:43:13 -0300

    Navigation tabs installed.

---
+++ src/navigation/index.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
+import {
+  TransitionPresets,
+  TransitionSpecs,
+  HeaderStyleInterpolators,
+} from 'react-navigation-stack';
+import Home, { homeNavigationOptions } from '../scenes/Home';
+import Detail, { detailNavigationOptions } from '../scenes/Detail';
+import Favorites, { favoritesNavigationOptions } from '../scenes/Favorites';
+import { CustomIcon } from '../components/atoms';
+import { Spacing, Colors, Typography } from '../styles';
+
+const HomeStack = createSharedElementStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: homeNavigationOptions,
+    },
+    Detail: {
+      screen: Detail,
+      navigationOptions: detailNavigationOptions,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      gestureEnabled: true,
+      cardOverlayEnabled: true,
+      headerTransparent: true,
+      headerTitle: '',
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+      ...TransitionPresets.FadeFromBottomAndroid,
+    },
+  },
+);
+
+const FavoriteStack = createSharedElementStackNavigator(
+  {
+    Favorite: {
+      screen: Favorites,
+      navigationOptions: favoritesNavigationOptions,
+    },
+    Detail: {
+      screen: Detail,
+      navigationOptions: detailNavigationOptions,
+    },
+  },
+  {
+    initialRouteName: 'Favorite',
+    defaultNavigationOptions: {
+      gestureEnabled: true,
+      cardOverlayEnabled: true,
+      headerTransparent: true,
+      headerTitle: '',
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+      ...TransitionPresets.FadeFromBottomAndroid,
+    },
+  },
+);
+
+const MainTabs = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeStack,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ focused, tintColor }) => {
+          return <CustomIcon name="home-variant" />;
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoriteStack,
+      navigationOptions: {
+        tabBarLabel: 'Favoritos',
+        tabBarIcon: ({ focused, tintColor }) => {
+          return <CustomIcon name="heart-outline" />;
+        },
+      },
+    },
+    More: {
+      screen: HomeStack,
+      navigationOptions: {
+        tabBarLabel: 'Mais',
+        tabBarIcon: ({ focused, tintColor }) => {
+          return <CustomIcon name="dots-horizontal" />;
+        },
+      },
+    },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      
+    }),
+    tabBarOptions: {
+      activeTintColor: Colors.PRIMARY,
+      inactiveTintColor: 'gray',
+      labelStyle: { fontSize: 14 },
+      style: {
+        backgroundColor: '#000',
+        //paddingTop: 20,
+      },
+    },
+  },
+);
+
+//const AppContainer = createAppContainer(HomeStack);
+const AppContainer = createAppContainer(MainTabs);
+export default AppContainer;
