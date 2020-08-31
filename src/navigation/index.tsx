3194f5c0dce91b7bb3003b6015266103ee5d3397
João Belem Jr <junior.jb@gmail.com>
2020-08-31 14:48:45 -0300

    Adjusting navigation labels

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,9 +3,11 @@ import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 import { TransitionPresets } from 'react-navigation-stack';
+import { I18n } from '@aws-amplify/core';
 import Home, { homeNavigationOptions } from '../scenes/Home';
 import Detail, { detailNavigationOptions } from '../scenes/Detail';
 import Favorites, { favoritesNavigationOptions } from '../scenes/Favorites';
+import More, { moreNavigationOptions } from '../scenes/More';
 import { CustomIcon } from '../components/atoms';
 import { Spacing, Colors, Typography } from '../styles';
 
@@ -69,12 +71,38 @@ const FavoriteStack = createSharedElementStackNavigator(
   },
 );
 
+const MoreStack = createSharedElementStackNavigator(
+  {
+    More: {
+      screen: More,
+      navigationOptions: favoritesNavigationOptions,
+    },
+  },
+  {
+    initialRouteName: 'More',
+    defaultNavigationOptions: {
+      gestureEnabled: true,
+      cardOverlayEnabled: true,
+      headerTransparent: true,
+      headerTitle: '',
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+      ...TransitionPresets.FadeFromBottomAndroid,
+    },
+  },
+);
+
 const MainTabs = createBottomTabNavigator(
   {
     Home: {
       screen: HomeStack,
       navigationOptions: {
-        tabBarLabel: 'Home',
+        tabBarLabel: I18n.get('Home'),
         tabBarIcon: ({ focused, tintColor }) => {
           return <CustomIcon name="home-variant" />;
         },
@@ -83,16 +111,16 @@ const MainTabs = createBottomTabNavigator(
     Favorites: {
       screen: FavoriteStack,
       navigationOptions: {
-        tabBarLabel: 'Favoritos',
+        tabBarLabel: I18n.get('Favorites'),
         tabBarIcon: ({ focused, tintColor }) => {
           return <CustomIcon name="heart-outline" />;
         },
       },
     },
     More: {
-      screen: HomeStack,
+      screen: MoreStack,
       navigationOptions: {
-        tabBarLabel: 'Mais',
+        tabBarLabel: I18n.get('More'),
         tabBarIcon: ({ focused, tintColor }) => {
           return <CustomIcon name="dots-horizontal" />;
         },
@@ -100,9 +128,6 @@ const MainTabs = createBottomTabNavigator(
     },
   },
   {
-    defaultNavigationOptions: ({ navigation }) => ({
-      
-    }),
     tabBarOptions: {
       activeTintColor: Colors.WHITE,
       inactiveTintColor: Colors.GRAY_DARK,
