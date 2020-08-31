680798432869bb00e4f15d43d3d4c939613dd0cd
João Belem Jr <junior.jb@gmail.com>
2020-08-31 14:45:29 -0300

    Add crash handler.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Platform, NativeModules } from 'react-native';
+import { Platform, NativeModules, Alert } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { I18n } from '@aws-amplify/core';
+import {
+  setJSExceptionHandler,
+  setNativeExceptionHandler,
+} from 'react-native-exception-handler';
 import Router from './navigation';
 import { store, persistor } from './store';
-//import { SpinnerLoading } from '_molecules';
+
+interface ErrorHandler {
+  e: object;
+  isFatal: boolean;
+}
 
 /* Import disctionaries */
 import portuguese from './config/i18n/i18n_pt_BR';
@@ -23,7 +31,32 @@ const setLanguage = () => {
   I18n.setLanguage(lang);
 };
 
+/* Crash handler */
+const errorHandler = (e: any, isFatal: boolean): void => {
+  if (isFatal) {
+    Alert.alert(
+      'Unexpected error occurred',
+      `
+        Error: ${isFatal ? 'Fatal:' : ''} ${e.name} ${e.message}
+        We have reported this to our team ! Please close the app and start again!
+        `,
+      [
+        {
+          text: 'Close',
+        },
+      ],
+    );
+  } else {
+    //console.log(e);
+  }
+};
+
 const App: React.FC = () => {
+  setJSExceptionHandler(errorHandler, true);
+  setNativeExceptionHandler((errorString) => {
+    console.log('setNativeExceptionHandler');
+    console.log(errorString);
+  });
   setLanguage();
   //persistor.purge(); //Para limpar a store
   return (
